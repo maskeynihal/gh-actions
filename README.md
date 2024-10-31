811b460cab359906f837041d5472caf151455973
maskeynihal <26411488+maskeynihal@users.noreply.github.com>
2024-10-31 10:01:06 +0545

    chore: add convert environment value from string to json

---
+++ README.md
@@ -0,0 +1,1 @@
+# All github action expression
---
+++ scripts/export_json_key_value_to_environment_variable.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Function to convert JSON string to key-value pairs
+json_to_key_value_pairs() {
+  local json_string="$1"
+
+  # Extract key-value pairs using jq and store them in a string
+  local kv_pairs=$(echo "$json_string" | jq -r 'to_entries | .[] | "\(.key)=\(.value)"')
+
+  # Output the key-value pairs
+  echo "$kv_pairs"
+}
+
+# Main function to handle the input JSON and export variables
+process_json() {
+  local json_string="$1"
+
+  echo "Input JSON: $json_string"
+
+  # Call the function and store the key-value pairs
+  local key_value_pairs=$(json_to_key_value_pairs "$json_string")
+
+  # Loop through the key-value pairs and set variables
+  while IFS= read -r pair; do
+
+    echo "Pair: $pair"
+    # Split the pair into key and value
+    IFS='=' read -r key value <<< "$pair"
+
+    echo "Key: $key"
+
+    # Sanitize the key by replacing '.' with '_'
+    sanitized_key=$(echo "$key" | sed 's/\./_/g')
+
+    # Export each key-value pair as a variable
+    export "$sanitized_key=$value"
+
+    # Processed variable
+    echo "Processed Variable: $sanitized_key=$value"
+
+    if [ -n "$GITHUB_ACTIONS" ]; then
+      echo "Running in GitHub Actions"
+
+      echo "${sanitized_key}"="${value}" >> "$GITHUB_ENV"
+    fi
+
+  done <<< "$key_value_pairs"
+}
+
+# Check if a JSON string was passed as an argument
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 '<json_string>'"
+
+  exit 0
+fi
+
+# Call the process_json function with the provided JSON string
+process_json "$1"
